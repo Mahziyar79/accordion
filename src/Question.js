ed9937eeb182bb31185428fd153f1bf98a448b26
mahziyarGlp <mgillanpour79@gmail.com>
2021-09-06 10:29:26 +0430

    first version accordion

--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Question = (question) => {
   const [showItem, setShowItem] = useState(false);
-  console.log(question);
 
     return ( 
-        <div>
+        <div className="accordion">
           <div className="accordion-header">
             <h3>{question.title}</h3>
             <button className="btn" onClick={() => setShowItem(!showItem)}>
-              +
+              {!showItem ? <AiOutlinePlus /> : <AiOutlineMinus />}
             </button>
           </div>
           {showItem && <p>{question.info}</p>}
